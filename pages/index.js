79622dc1cae1492b7ca3741b3f06ad8ccffbe87e
MO20523 <abualfoulmotasem99@gmail.com>
2025-04-10 22:40:56 +0300

    Added default export to index.js

---
+++ pages/index.js
@@ -0,0 +1,10 @@
+// pages/index.js
+export default function Home() {
+    return (
+      <div style={{ textAlign: 'center', padding: '50px' }}>
+        <h1>Welcome to RAP Mining App</h1>
+        <p>Connected successfully! 🚀</p>
+      </div>
+    );
+  }
+  
